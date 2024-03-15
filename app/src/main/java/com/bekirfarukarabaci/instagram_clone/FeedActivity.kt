8c9666196aaace68d519a01e1482f5f2ab88e32e
Bekir Faruk ARABACI <113997401+BekirFarukARABACI@users.noreply.github.com>
2024-03-16 02:11:42 +0300

    feat : çıkış işlemleri ve menü oluşturma

--- app/src/main/java/com/bekirfarukarabaci/instagram_clone/FeedActivity.kt
+++ app/src/main/java/com/bekirfarukarabaci/instagram_clone/FeedActivity.kt
@@ -1,15 +1,23 @@
 package com.bekirfarukarabaci.instagram_clone
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.bekirfarukarabaci.instagram_clone.databinding.ActivityFeedBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class FeedActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +25,32 @@ class FeedActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = Firebase.auth
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.insta_menu,menu)
+
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (item.itemId == R.id.add_post){
+            val intent = Intent(this,UploadActivity::class.java)
+            startActivity(intent)
+        }
+        else if (item.itemId == R.id.signout){
+            auth.signOut()
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
